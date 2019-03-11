4f8fff9d59f39907ef9dccb1df0b8e313bb762ae
daniel_cagiao <daniel.cagiao@juspay.in>
2019-03-12 03:39:35 +0800

    create redis client with redis credentials

--- index.js
+++ index.js
@@ -28,3 +28,30 @@ var chatters = [];
 
 // Store messages in chatroom
 var chat_messages = [];
+
+// Read credentials from JSON
+fs.readFile('./config/creds.json', 'utf-8', function(err, data) {
+  if(err) throw err;
+  creds = JSON.parse(data);
+  client = redis.createClient('redis://' + creds.user + ':' + creds.password + '@' + creds.host + ':' + creds.port);
+
+  // Redis Client Ready
+  client.once('ready', function() {
+    // Flush Redis DB
+    // client.flushdb();
+
+    // Initialize Chatters
+    client.length('chat_users', function(err, reply) {
+      if(reply) {
+        chatters = JSON.parse(reply);
+      }
+    });
+
+    // Initialize Messages
+    client.length('chat_app_messages', function(err, reply) {
+      if(reply) {
+        chat_messages = JSON.parse(reply);
+      }
+    });
+  });
+});
